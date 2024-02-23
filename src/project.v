7252f745fb2bfae3cf41abd1120a3bee48f24efd
matztron <muschmatthias@gmail.com>
2024-02-23 22:09:04 +0100

    changed top-level name to tt_um_... to comply with tinyTapeout naming :)

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module TT_MATTHIAS_M_PAL_TOP_WRAPPER (
+module tt_um_MATTHIAS_M_PAL_TOP_WRAPPER (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
